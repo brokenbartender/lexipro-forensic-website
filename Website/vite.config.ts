17b6fbf36c350f09d6775c195a947c32b9977687
brokenbartender <codymckenzie23@gmail.com>
2026-01-12 14:25:32 -0500

    Update vite.config.ts

--- Website/vite.config.ts
+++ Website/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, (process as any).cwd(), '');
-  
+
   return {
     // This 'base' line is what makes the site work on GitHub Pages
     base: '/lexipro-forensic-website/', 
